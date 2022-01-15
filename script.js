63c60f8451d300bd2ba433c35c67543132136008
Nelson Peixoto <nelsonpeixoto@hotmail.com.br>
2022-01-15 01:25:24 -0300

    Implementar arquivo javascript mudando a primeira imagem

---
+++ script.js
@@ -0,0 +1,28 @@
+const imagem = document.querySelector('#img');
+const botao = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#nome');
+const especie = document.querySelector('#especie');
+const condicao = document.querySelector('#status');
+
+pegarPersonagem = () => {
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'aplication/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem .alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = data.status;
+    });
+}
+
+botao.addEventListener('click', function() {
+  pegarPersonagem();
+})
+
+
